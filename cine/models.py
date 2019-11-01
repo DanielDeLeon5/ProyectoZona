a3926d76507491d973a7afdb4428741e3321f467
DanielDeLeon5 <danieldleon5@outlook.com>
2019-10-31 21:58:36 -0600

    Modelos creados

--- cine/models.py
+++ cine/models.py
@@ -17,4 +17,23 @@ class Pelicula(models.Model):
 
     def __str__(self):
         return self.title
-# Create your models here.
+
+
+class Sala(models.Model):
+    id = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    asientos = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+class Funcion(models.Model):
+    id = models.AutoField(primary_key = True)
+    descripcion = models.TextField()
+    horario = models.TextField()
+    pelicula_id = models.ForeignKey(Pelicula, on_delete = models.CASCADE)
+    sala_id = models.ManyToManyField(Sala)
+
+    def __str__(self):
+        return self.horario
