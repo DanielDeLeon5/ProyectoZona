9c2326b7ceb8893a3d90deb7442c6dfa6a31bb9b
DanielDeLeon5 <danieldleon5@outlook.com>
2019-11-02 15:29:14 -0600

    Cruds completos

--- cine/urls.py
+++ cine/urls.py
@@ -1,10 +1,25 @@
 from django.urls import path
 from . import views
-from .views import Home, crearPelicula, crearSala, crearFuncion
-
+from .views import Home, crearPelicula, crearSala, crearFuncion, listarPelicula
+from .views import listarSala, listarFuncion
+from .views import editarPelicula, editarSala, editarFuncion
+from .views import eliminarPelicula, eliminarSala, eliminarFuncion
 urlpatterns = [
     path('', Home, name='index'),
     path('crear_pelicula/',crearPelicula, name = 'crear_pelicula'),
     path('crear_sala/',crearSala, name = 'crear_sala'),
     path('crear_funcion/',crearFuncion, name = 'crear_funcion'),
+
+    path('listar_pelicula/',listarPelicula, name = 'listar_pelicula'),
+    path('listar_sala/',listarSala, name = 'listar_sala'),
+    path('listar_funcion/',listarFuncion, name = 'listar_funcion'),
+
+    path('editar_pelicula/<int:id>',editarPelicula, name = 'editar_pelicula'),
+    path('editar_sala/<int:id>',editarSala, name = 'editar_sala'),
+    path('editar_funcion/<int:id>',editarFuncion, name = 'editar_funcion'),
+
+    path('eliminar_pelicula/<int:id>',eliminarPelicula, name = 'eliminar_pelicula'),
+    path('eliminar_sala/<int:id>',eliminarSala, name = 'eliminar_sala'),
+    path('eliminar_funcion/<int:id>',eliminarFuncion, name = 'eliminar_funcion'),
+
 ]
--- cine/views.py
+++ cine/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.exceptions import ObjectDoesNotExist
 from django.utils import timezone
-from .models import Pelicula, Funcion
+from .models import Pelicula, Funcion, Sala
 from .forms import PeliculaForm, SalaForm, FuncionForm
 
 def Home(request):
@@ -35,3 +36,81 @@ def crearFuncion(request):
     else:
         funcion_form = FuncionForm()
     return render(request,'cine/crear_funcion.html',{'funcion_form':funcion_form})
+
+def listarPelicula(request):
+    peliculas = Pelicula.objects.all()
+    return render(request,'cine/listar_pelicula.html',{'peliculas':peliculas})
+
+def listarSala(request):
+    salas = Sala.objects.all()
+    return render(request,'cine/listar_sala.html',{'salas':salas})
+
+def listarFuncion(request):
+    funciones = Funcion.objects.all()
+    return render(request,'cine/listar_funcion.html',{'funciones':funciones})
+
+def editarPelicula(request,id):
+    pelicula_form = None
+    error = None
+    try:
+        pelicula = Pelicula.objects.get(id = id)
+        if request.method == 'GET':
+            pelicula_form = PeliculaForm(instance = pelicula)
+        else:
+            pelicula_form = PeliculaForm(request.POST, instance = pelicula)
+            if pelicula_form.is_valid():
+                pelicula_form.save()
+            return redirect('listar_pelicula')
+    except ObjectDoesNotExist as e:
+        error = e
+
+    return render(request, 'cine/crear_pelicula.html',{'pelicula_form':pelicula_form,'error':error})
+
+def editarSala(request,id):
+    sala_form = None
+    error = None
+    try:
+        sala = Sala.objects.get(id = id)
+        if request.method == 'GET':
+            sala_form = SalaForm(instance = sala)
+        else:
+            sala_form = SalaForm(request.POST, instance = sala)
+            if sala_form.is_valid():
+                sala_form.save()
+            return redirect('listar_sala')
+    except ObjectDoesNotExist as e:
+        error = e
+
+    return render(request, 'cine/crear_sala.html',{'sala_form':sala_form,'error':error})
+
+def editarFuncion(request,id):
+    funcion_form = None
+    error = None
+    try:
+        funcion = Funcion.objects.get(id = id)
+        if request.method == 'GET':
+            funcion_form = FuncionForm(instance = funcion)
+        else:
+            funcion_form = FuncionForm(request.POST, instance = funcion)
+            if funcion_form.is_valid():
+                funcion_form.save()
+            return redirect('listar_funcion')
+    except ObjectDoesNotExist as e:
+        error = e
+
+    return render(request, 'cine/crear_funcion.html',{'funcion_form':funcion_form,'error':error})
+
+def  eliminarPelicula(request,id):
+    pelicula = Pelicula.objects.get(id = id)
+    pelicula.delete()
+    return redirect('listar_pelicula')
+
+def  eliminarSala(request,id):
+    sala = Sala.objects.get(id = id)
+    sala.delete()
+    return redirect('listar_sala')
+
+def  eliminarFuncion(request,id):
+    funcion = Funcion.objects.get(id = id)
+    funcion.delete()
+    return redirect('listar_funcion')
